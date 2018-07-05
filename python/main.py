2a4f25622e0c8a033811b0089dcd333b85f3ca1d
MarinaSakai <pavane.ravel.5m2t0.conamore@gmail.com>
2018-07-05 11:01:50 +0900

    main.py変更

--- python/main.py
+++ python/main.py
@@ -2,21 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import webapp2
-import os
-import jinja2
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
-
-class BaseHandler(webapp2.RequestHandler):
-    def render(self, html, values={}):
-        template = JINJA_ENVIRONMENT.get_template(html)
-        self.response.write(template.render(values))
-
-class MainHandler(BaseHandler):
-    def get(self):
-        self.render("main.html")
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
