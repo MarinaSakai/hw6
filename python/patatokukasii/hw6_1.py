14ba982d2cad931a9228b10d2d86c1cbfe5d1a16
MarinaSakai <pavane.ravel.5m2t0.conamore@gmail.com>
2018-07-05 12:06:14 +0900

    update hw6_1.py

---
+++ python/patatokukasii/hw6_1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import webapp2
+import os
+import jinja2
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, html, values={}):
+        template = JINJA_ENVIRONMENT.get_template(html)
+        self.response.write(template.render(values))
+
+class MainHandler(BaseHandler):
+    def get(self):
+        self.render("hw6_1.html")
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+], debug=True)
